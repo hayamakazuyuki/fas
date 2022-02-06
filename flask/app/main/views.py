7342f09fabff59fced1de3157f97ecd3d636442f
hayamakazuyuki <hayama@sfinter.com>
2022-02-06 20:19:24 +0900

    updated: index page

--- flask/app/main/views.py
+++ flask/app/main/views.py
@@ -1,23 +1,31 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from datetime import datetime, timedelta, timezone, date
+from flask import Blueprint, render_template, request
+from flask_login import current_user, login_required
+from sqlalchemy import func
 
-from ..models import Product
+from ..models import Product, ProductOrder
 
 from app import db
 
 main = Blueprint('main', __name__)
+JST = timezone(timedelta(hours=+9), 'JST')
 
 
+# index: sf.furoshiki.in
 @main.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    orders = ProductOrder.query.filter(ProductOrder.sales_by == current_user.id).order_by(ProductOrder.id.desc()).paginate(page=page, per_page=20)
+    today = date.today()
 
+    sum_qty_today = db.session.query(func.sum(ProductOrder.qty)).filter(ProductOrder.sales_by ==current_user.id)\
+        .filter(func.date(ProductOrder.date) == today).filter(ProductOrder.item != 901).scalar()
 
-@main.route('/test')
-@login_required
-def test():
-    return 'テスト'
+    sum_amount_today = db.session.query(func.sum((ProductOrder.price * ProductOrder.qty)))\
+        .filter(ProductOrder.sales_by == current_user.id).filter(func.date(ProductOrder.date) == today).scalar()
+
+    return render_template('index.html', orders=orders, today=today, sum_qty_today=sum_qty_today, sum_amount_today=sum_amount_today)
 
 
 # show products/items list
