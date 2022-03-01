a4eb6d1971c6f15a7624b0d4c131e22972efc326
hayamakazuyuki <hayama@sfinter.com>
2022-03-02 02:18:28 +0900

    customer order input updated

--- customer/app/static/js/order.js
+++ customer/app/static/js/order.js
@@ -1,14 +1,12 @@
 // ------- grab selects, inputs and checkboxes ------- //
 const item = document.getElementById('item');
-const qty = document.getElementById('qty');
 
 // ------------grab Modal targets --------------- //
 const mItem = document.getElementById('mItem');
-const mQty = document.getElementById('mQty');
 
 // --------- copy selects, inputs and checkboxes to modal -------- //
 
-let selectToModal = (target, value, text) => {
+let selectToModal = (target, text) => {
     target.textContent = text;
 };
 
