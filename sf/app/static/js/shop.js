cd273a561b9a3fcea889b1cd885b6c319a0811c1
hayamakazuyuki <hayama@sfinter.com>
2022-11-12 22:56:47 +0900

    sf updated

--- sf/app/static/js/shop.js
+++ sf/app/static/js/shop.js
@@ -4,18 +4,15 @@ form.addEventListener('submit', (e) => {
 
     // regular expressions;
     const reNumOnly = /^[0-9]+$/; //半角数字（空白OK）の正規表現
-    const reId = /^[1-9][0-9]*/;
     const reTel = /\d{2,4}-\d{2,4}-\d{2,4}$/; //数字（2〜4桁）-数字（2〜4桁）-数字（2〜4桁）
     const reTelNumOnly = /^0[1-9][0-9]{8,}/; //0から始まり1~9、そして数字のみ８つ以上
 
     // values for validation;
-    let id = document.getElementById('id').value; //事業所ID
     let zip = document.getElementById('zip').value; //郵便番号
     let telephone = document.getElementById('telephone').value; //電話番号
     let tel_num = telephone.replace(/-/g,'');
 
     // elements to display error messages;
-    const error_id = document.getElementById('error_id'); // 事業所ID
     const error_zip = document.getElementById('error_zip'); // 郵便番号
     const error_tel = document.getElementById('error_tel'); // 電話番号
 
@@ -25,9 +22,7 @@ form.addEventListener('submit', (e) => {
 
     e.preventDefault();
 
-    if (!id.match(reId)) {
-        return error_id.textContent = errorNumOnly;
-    } else if (!zip.match(reNumOnly)) {
+    if (!zip.match(reNumOnly)) {
         return error_zip.textContent = errorNumOnly;
     } else if (!telephone.match(reTel)) {
         return error_tel.textContent = errorTel;
