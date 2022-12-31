8131975d1f72eaf157d9660c4bdece36fb78ec4a
hayamakazuyuki <hayama@sfinter.com>
2022-12-31 12:00:24 +0900

    ✅ downloads updated

--- sf/app/downloads/views.py
+++ sf/app/downloads/views.py
@@ -1,10 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
+import datetime
 
 downloads = Blueprint('downloads', __name__, url_prefix='/downloads')
+JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
 
 
 @downloads.route('/')
 @login_required
 def index():
-    return render_template('downloads/index.html')
+    target = request.args.get("target")
+
+    if target is None:
+        target = datetime.datetime.now(JST)
+
+    return render_template('downloads/index.html', target=target)
