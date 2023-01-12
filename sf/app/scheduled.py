d4a61726d4463f526f341649be7f0512b39384dd
hayamakazuyuki <hayama@sfinter.com>
2023-01-12 15:05:43 +0900

    ✅ daily report to be beta

--- sf/app/scheduled.py
+++ sf/app/scheduled.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 import datetime
 import requests
 import os
+from .calcs import get_total_qty, get_total_amount
 
 scheduled = Blueprint('scheduled', __name__)
 
@@ -12,17 +13,25 @@ now = datetime.datetime.now(JST)
 @scheduled.route('/daily-report')
 def daily_report():
 
+    yesterday = now - datetime.timedelta(days=1)
+
     api_key = os.environ.get('X_CHATWORK_TOKEN')
     url = "https://api.chatwork.com/v2/rooms/281437593/messages"
-    body = render_template('daily-report.txt', now=now)
-    payload = "self_unread=1"
 
     headers = {
         "x-chatworktoken": api_key
         }
+
+    payload = "self_unread=1"
+
+    total_qty = get_total_qty(yesterday)
+    total_amount = get_total_amount(yesterday)
+
+    body = render_template('daily-report.txt', now=now, yesterday=yesterday, total_qty=total_qty, total_amount=total_amount)
 
     params = { "body" : body }
 
     response = requests.post(url, headers=headers, data=payload, params=params)
 
     return ('', 204)
+    # return body
