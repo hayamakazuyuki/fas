d0c32d551b1e6d99084d64ff794e8bb65c2581f7
hayamakazuyuki <hayama@sfinter.com>
2023-01-13 18:59:57 +0900

    ✅ daily-report updated

--- sf/app/scheduled.py
+++ sf/app/scheduled.py
@@ -15,6 +15,11 @@ def daily_report():
 
     yesterday = now - datetime.timedelta(days=1)
 
+    total_qty = get_total_qty(yesterday)
+    total_amount = get_total_amount(yesterday)
+
+    body = render_template('daily-report.txt', now=now, yesterday=yesterday, total_qty=total_qty, total_amount=total_amount)
+
     api_key = os.environ.get('X_CHATWORK_TOKEN')
     url = "https://api.chatwork.com/v2/rooms/281437593/messages"
 
@@ -22,16 +27,11 @@ def daily_report():
         "x-chatworktoken": api_key
         }
 
-    payload = "self_unread=1"
+    # payload = "self_unread=1"
 
-    total_qty = get_total_qty(yesterday)
-    total_amount = get_total_amount(yesterday)
+    params = { "self_unread" : 1,
+                "body" : body }
 
-    body = render_template('daily-report.txt', now=now, yesterday=yesterday, total_qty=total_qty, total_amount=total_amount)
-
-    params = { "body" : body }
-
-    response = requests.post(url, headers=headers, data=payload, params=params)
+    response = requests.post(url, headers=headers, params=params)
 
     return ('', 204)
-    # return body
