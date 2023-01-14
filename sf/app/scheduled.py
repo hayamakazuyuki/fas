4f20d0717117416633bad0acd4c9fc33b3a371f2
hayamakazuyuki <hayama@sfinter.com>
2023-01-14 21:13:33 +0900

    ✅ daily report updated

--- sf/app/scheduled.py
+++ sf/app/scheduled.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 import datetime
 import requests
 import os
-from .calcs import get_total_qty, get_total_amount
+from .calcs import get_total_qty, get_total_amount, get_sum_by_item, get_sum_by_staff
 
 scheduled = Blueprint('scheduled', __name__)
 
@@ -15,10 +15,16 @@ def daily_report():
 
     yesterday = now - datetime.timedelta(days=1)
 
+    # summary
     total_qty = get_total_qty(yesterday)
     total_amount = get_total_amount(yesterday)
 
-    body = render_template('daily-report.txt', now=now, yesterday=yesterday, total_qty=total_qty, total_amount=total_amount)
+    # group by item and staff
+    # sum_by_item = get_sum_by_item(yesterday)
+    sum_by_staff = get_sum_by_staff(yesterday)
+
+    body = render_template('daily-report.txt', now=now, yesterday=yesterday, total_qty=total_qty, total_amount=total_amount, 
+            sum_by_staff=sum_by_staff)
 
     api_key = os.environ.get('X_CHATWORK_TOKEN')
     url = "https://api.chatwork.com/v2/rooms/281437593/messages"
