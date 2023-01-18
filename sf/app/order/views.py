b91e1079f449f8f8be80920568e3e9b63663782c
hayamakazuyuki <hayama@sfinter.com>
2023-01-19 08:25:34 +0900

    🐛 order register delivery date updated

--- sf/app/order/views.py
+++ sf/app/order/views.py
@@ -60,7 +60,6 @@ def register(customer_id, id):
         order.item = request.form['item']
         order.price = request.form['price']
         order.qty = request.form['qty']
-        # order.delivery_check = request.form.get('noDelivery')
 
         db.session.add(order)
         db.session.commit()
@@ -87,7 +86,6 @@ def register(customer_id, id):
             order2.item = request.form['item2']
             order2.price = request.form['price2']
             order2.qty = request.form['qty2']
-            # order2.delivery_check = request.form.get('noDelivery2')
 
             db.session.add(order2)
             db.session.commit()
@@ -114,7 +112,6 @@ def register(customer_id, id):
             order3.item = request.form['item3']
             order3.price = request.form['price3']
             order3.qty = request.form['qty3']
-            # order3.delivery_check = request.form.get('noDelivery3')
 
             db.session.add(order3)
             db.session.commit()
@@ -167,7 +164,7 @@ def register_with_items(customer_id, id):
 
     # generate min and max dates for delivery date request.
     min_date = datetime.now(JST) + timedelta(days=4)
-    max_date = datetime.now(JST) + timedelta(days=29)
+    max_date = datetime.now(JST) + timedelta(days=15)
 
     if request.method == 'POST':
         staff = shop.customer.staff
